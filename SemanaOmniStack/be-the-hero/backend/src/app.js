d2d28ef2c3a51797f644eaec0216e479fad12900
Simone Auler <auler.simone@gmail.com>
2020-03-29 16:54:15 -0300

    desenvolvido validações e testes no backend

--- SemanaOmniStack/be-the-hero/backend/src/app.js
+++ SemanaOmniStack/be-the-hero/backend/src/app.js
@@ -25,6 +25,7 @@
   
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes'); /** O ponto é para referenciar a mesma pasta */
 
 const app = express();
@@ -32,8 +33,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
-
-
-app.listen(3333);
+module.exports = app;
 
